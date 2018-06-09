6573ee87ec0cd0ba7e51043bc285475f4730d06a
Seth Bergman <seth.atxwebs@gmail.com>
2018-06-09 02:36:04 -0500

    updates

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-const Repos = ({repos}) => {
+const Repos = ({ repos }) => {
   return (
     <div>
       <h3> User Repos </h3>
-      <ul className='list-group'>
+      <ul className="list-group">
         {repos.map((repo, index) => {
           return (
-            <li className='list-group-item' key={repo.name}>
-              {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+            <li className="list-group-item" key={repo.name}>
+              {repo.html_url && (
+                <h4>
+                  <a href={repo.html_url}>{repo.name}</a>
+                </h4>
+              )}
               {repo.description && <p>{repo.description}</p>}
             </li>
           )
@@ -20,7 +24,7 @@ const Repos = ({repos}) => {
 
 Repos.propTypes = {
   username: React.PropTypes.string.isRequired,
-  repos: React.PropTypes.array.isRequired
+  repos: React.PropTypes.array.isRequired,
 }
 
 export default Repos
